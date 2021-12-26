98567f4c19cdc35d194e5cec77f42217b82661ef
EdwardLeeMacau <b05901119@g.ntu.edu.tw>
2021-12-26 13:19:28 +0800

    Provide mt19937ar in C ABI

--- sga-cpp/mt19937ar.h
+++ sga-cpp/mt19937ar.h
@@ -3,12 +3,25 @@
   E-mail: tianliyu@nextumi.com
 ********************************/
 
-extern void init_by_array (unsigned long init_key[], int key_length);
+#ifndef MT19937AR_H_
+#define MT19937AR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_by_array (unsigned long init_key[], int key_length);
                                  //[0,1]
-extern double genrand_real1 (void);
+double genrand_real1 (void);
                                  //[0,1)
-extern double genrand_real2 (void);
+double genrand_real2 (void);
                                  //(0,1)
-extern double genrand_real3 (void);
+double genrand_real3 (void);
                                  //[0,1)
-extern double genrand_res53 (void);
+double genrand_res53 (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
